b5c6f9484bcb6c7c354ba45666f65ef1e10d4e6d
therealBudder <80362332+therealBudder@users.noreply.github.com>
2025-03-11 10:13:27 +0000

    golem returns to starting position on losing sight of the player

--- Assets/Scripts/EnemyMovementController.cs
+++ Assets/Scripts/EnemyMovementController.cs
@@ -9,6 +9,7 @@ public class EnemyMovementController : MonoBehaviour {
     public Transform target;
     public float attackDistance;
     
+    private Vector3 startingPosition;
     private NavMeshAgent agent;
     private Animator animator;
     private float distance;
@@ -19,23 +20,40 @@ public class EnemyMovementController : MonoBehaviour {
     {
         animator = GetComponent<Animator>();
         agent = GetComponent<NavMeshAgent>();
+        startingPosition = agent.transform.position;
+        print(startingPosition);
     }
 
     // Update is called once per frame
     void Update() {
 
         distance = Vector3.Distance(agent.transform.position, target.position);
-
+        NavMeshPath navMeshPath = new NavMeshPath();
+        
         if (distance < attackDistance) {
             agent.isStopped = true;
             animator.SetBool("Attack", true);
             animator.SetBool("Walk", false);
         }
-        else {
+        else if (agent.CalculatePath(target.position, navMeshPath) && navMeshPath.status == NavMeshPathStatus.PathComplete) {
+            // agent.SetPath(navMeshPath);
             agent.isStopped = false;
             animator.SetBool("Attack", false);
             animator.SetBool("Walk", true);
             agent.destination = target.position;
+            
+        }
+        else if (Vector3.Distance(agent.transform.position, startingPosition) < 1) {
+            
+            agent.isStopped = true;
+            animator.SetBool("Walk", false);
+            
+        }
+        else {
+            agent.isStopped = false;
+            animator.SetBool("Walk", true);
+            animator.SetBool("Attack", false);
+            agent.destination = startingPosition;
         }
 
     }
